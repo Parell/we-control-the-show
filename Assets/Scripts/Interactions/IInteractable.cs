71f036f90829cd301d479e3679beed1c6a8811da
Parell <parelldev01@gmail.com>
2022-08-01 15:37:42 -0500

    Prep

--- Assets/Scripts/Interactions/IInteractable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.Events;
-
-public interface IInteractable
-{
-    public UnityAction<IInteractable> OnInteractionComplete { get; set; }
-
-    public void Interact(Interactor interactor, out bool interactSuccessful);
-    public void EndInteraction();
-}
--- Assets/Scripts/Inventory/ReadOnlyAttribute.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-/// <summary> Attribute is used only to mark Readonly properties </summary>
-
-public class ReadOnlyAttribute : PropertyAttribute { }
